753898abf748961676a72efa765f643ee81f7cc9
Christian Potemski <c.potemski@gmail.com>
2023-12-11 13:46:05 +0100

    day 10 finished - not beautiful but working :D

--- 10/2.js
+++ 10/2.js
@@ -1,4 +1,5 @@
 const {readFile, writeFile} = require('node:fs/promises');
+const {arrayUnique} = require("../utils");
 
 
 const DIRECTIONS = {
@@ -12,45 +13,67 @@ const DIRECTIONS_MOVEMENT = {
     UP: {
         x: 0,
         y: -1,
+        inside: {
+            x: 1,
+            y: 0
+        }
     },
     DOWN: {
         x: 0,
         y: 1,
+        inside: {
+            x: -1,
+            y: 0
+        }
     },
     LEFT: {
         x: -1,
         y: 0,
+        inside: {
+            x: 0,
+            y: -1
+        }
     },
     RIGHT: {
         x: 1,
         y: 0,
+        inside: {
+            x: 0,
+            y: 1
+        }
     },
 }
 
 const PIPES = {
+    'S': {
+        [DIRECTIONS.UP]: DIRECTIONS.UP,
+        [DIRECTIONS.DOWN]: DIRECTIONS.DOWN,
+        [DIRECTIONS.LEFT]: DIRECTIONS.LEFT,
+        [DIRECTIONS.RIGHT]: DIRECTIONS.RIGHT,
+    },
     '|': {
-        [DIRECTIONS.UP] : DIRECTIONS.UP,
-        [DIRECTIONS.DOWN] : DIRECTIONS.DOWN,
+        [DIRECTIONS.UP]: DIRECTIONS.UP,
+        [DIRECTIONS.DOWN]: DIRECTIONS.DOWN,
     },
     '-': {
-        [DIRECTIONS.LEFT] : DIRECTIONS.LEFT,
-        [DIRECTIONS.RIGHT] : DIRECTIONS.RIGHT,
+        [DIRECTIONS.LEFT]: DIRECTIONS.LEFT,
+        [DIRECTIONS.RIGHT]: DIRECTIONS.RIGHT,
     },
     'F': {
-        [DIRECTIONS.UP] : DIRECTIONS.RIGHT,
-        [DIRECTIONS.LEFT] : DIRECTIONS.DOWN,
+        [DIRECTIONS.UP]: DIRECTIONS.RIGHT,
+        [DIRECTIONS.LEFT]: DIRECTIONS.DOWN,
     },
     'L': {
-        [DIRECTIONS.DOWN] : DIRECTIONS.RIGHT,
-        [DIRECTIONS.LEFT] : DIRECTIONS.UP,
+        [DIRECTIONS.DOWN]: DIRECTIONS.RIGHT,
+        [DIRECTIONS.LEFT]: DIRECTIONS.UP,
     },
     'J': {
-        [DIRECTIONS.RIGHT] : DIRECTIONS.UP,
-        [DIRECTIONS.DOWN] : DIRECTIONS.LEFT,
+        [DIRECTIONS.RIGHT]: DIRECTIONS.UP,
+        [DIRECTIONS.DOWN]: DIRECTIONS.LEFT,
     },
     '7': {
-        [DIRECTIONS.UP] : DIRECTIONS.LEFT,
-        [DIRECTIONS.RIGHT] : DIRECTIONS.DOWN,
+        [DIRECTIONS.UP]: DIRECTIONS.LEFT,
+        [DIRECTIONS.RIGHT]: DIRECTIONS.DOWN,
     },
 }
 
@@ -68,69 +91,95 @@ const main = async () => {
         return {y, x: index}
     }).filter(a => !!a)[0];
 
-    console.log(start);
+    // console.log('start', start);
 
-    const currentPipes = [];
+    let currentPipe = {
+        pipeType: '',
+        ...start
+    };
     const pipeSegements = [`${start.x}-${start.y}`];
+    const insideSegments = [];
 
-    const traverse = (pipe) => {
-        const newX = pipe.x + DIRECTIONS_MOVEMENT[pipe.direction].x;
-        const newY = pipe.y + DIRECTIONS_MOVEMENT[pipe.direction].y;
-        if(newX < 0 || newY < 0 || newX >= lines[0].length || newY >= lines.length) {
+    const traverse = () => {
+        const newX = currentPipe.x + DIRECTIONS_MOVEMENT[currentPipe.direction].x;
+        const newY = currentPipe.y + DIRECTIONS_MOVEMENT[currentPipe.direction].y;
+        let insideX = newX + DIRECTIONS_MOVEMENT[currentPipe.direction].inside.x;
+        let insideY = newY + DIRECTIONS_MOVEMENT[currentPipe.direction].inside.y;
+
+        if (newX < 0 || newY < 0 || newX >= lines[0].length || newY >= lines.length) {
             return;
         }
         const pipeType = lines[newY][newX];
-        if(Object.keys(PIPES).includes(pipeType) && PIPES[pipeType][DIRECTIONS[pipe.direction]]) {
-            currentPipes.push({
+        const newDirection = PIPES[pipeType][DIRECTIONS[currentPipe.direction]]
+        if (Object.keys(PIPES).includes(pipeType) && newDirection) {
+            currentPipe = {
                 pipeType,
                 y: newY,
                 x: newX,
-                direction: PIPES[pipeType][DIRECTIONS[pipe.direction]]
-            })
+                direction: newDirection
+            }
             pipeSegements.push(`${newX}-${newY}`);
+
+            insideSegments.push(`${insideX}-${insideY}`);
+            insideX = newX + DIRECTIONS_MOVEMENT[newDirection].inside.x;
+            insideY = newY + DIRECTIONS_MOVEMENT[newDirection].inside.y;
+            insideSegments.push(`${insideX}-${insideY}`);
         }
     }
 
     //findStartPipes
-    Object.keys(DIRECTIONS).forEach(direction => {
-        traverse({
-            pipeType: '',
-            ...start,
-            direction
-        })
-    })
+    for (let direction of Object.keys(DIRECTIONS)) {
+        if (currentPipe.pipeType) {
+            continue;
+        }
+        currentPipe.direction = direction
+        traverse()
+    }
 
+    // console.log('startPipe', currentPipe);
 
     let loopFinished = false;
     let count = 1;
     while (!loopFinished) {
-        if(currentPipes[0].x === currentPipes[1].x && currentPipes[0].y === currentPipes[1].y) {
-            loopFinished = true
-        }
-
-        traverse(currentPipes.shift());
+        traverse();
         count++;
         // console.log(currentPipes);
+        if (currentPipe.pipeType === 'S') {
+            loopFinished = true
+        }
     }
 
     // console.log(pipeSegements);
+    const filteredInside = insideSegments.filter(arrayUnique).filter(segment => !pipeSegements.includes(segment));
+    // console.log(filteredInside.length)
 
-    let areaData = '';
-    lines.forEach((line, y) => {
-        const chars = line.split('');
-        chars.forEach((char, x) => {
-            if(pipeSegements.find(segment => segment === `${x}-${y}`)) {
-                areaData += 'O';
+
+    const areaData = lines.map((line, y) => {
+        let newLine = line.split('').map((char, x) => {
+            if (pipeSegements.find(segment => segment === `${x}-${y}`)) {
+                return '#';
+            } else if (insideSegments.find(segment => segment === `${x}-${y}`)) {
+                return 'X';
             } else {
-                areaData += ' ';
+                return ' ';
             }
-        })
-        areaData += '\n';
+        }).join('')
+
+        const matches = [...newLine.matchAll(/X\s+X/g)];
+        if (matches.length > 0) {
+            matches.forEach(([match]) => {
+                newLine = newLine.replace(match, match.replaceAll(' ', 'X'))
+            })
+        }
+        return newLine;
     })
+        .join('')
+    //     .join('\n')
+    //
+    // await writeFile('area.txt', areaData);
 
-    await writeFile('area.txt', areaData);
+    console.log([...areaData.matchAll(/X/g)].length)
 
-    // console.log(count / 2)
 }
 
 main();
